559e5719d06f1a393851098014a99cde55f344b6
whatsmysuggestion <51940750+whatsmysuggestion@users.noreply.github.com>
2020-10-31 20:07:09 +0530

    Add files via upload
    CoreJava-Array Examples

---
+++ First.java
@@ -0,0 +1,45 @@
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		 * int a[]=new int[10];
+		 * 
+		 * int []b=new int[10]; int c[]=new int[' ']; int d[]={1,90,3,4,85,6};
+		 * System.out.println(c.length); System.out.println(d.length);
+		 * 
+		 * System.out.println(d[0]);
+		 * 
+		 * int k=d[4]; System.out.println(k);
+		 * 
+		 * d[4]=99;
+		 * 
+		 * System.out.println(d[4]);
+		 */
+		 
+		
+		
+		
+		  int a1[][]=new int[10][10];
+		  int []a2[]=new int[10][10];
+		  int a3[][]=new int[' '][10]; 
+		  int a4[][]=new int[10][' ']; 
+		  int a6[][]=new int[' '][' ']; 
+		  int a5[][]={{1,2},{3,4}};
+		  
+		  System.out.println(a5[0][0]);
+		  
+		  System.out.println(a5[1][1]);
+		  
+		  String s[]=new String[10];
+		 
+		 
+		 
+		 
+
+	}
+
+}
